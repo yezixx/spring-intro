9692ea554655df9c97fa2230541828f0f49e098a
yezixx <skylow917@daum.net>
2024-04-04 12:06:38 +0900

    API - @ResponseBody : HTTP의 BODY에 직접 내려줌 - @ResponseBody 후 객체를 반환하면 JSON으로 바꿔서 반환해줌, View 없이 반환

--- src/main/java/spring/springintro/controller/HelloController.java
+++ src/main/java/spring/springintro/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,31 @@ public class HelloController {
     public String helloMvc(@RequestParam(value="name", required = false) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http body에 데이터를 직접 넣어주겠다는 뜻
+    public String helloSpring(@RequestParam("name") String name){
+        return "hello " +name; // view 없이 문자가 그대로 내려감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체 return -> JSON으로 출력
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
